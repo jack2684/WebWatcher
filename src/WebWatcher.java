6ca762a8c9722a785319b1aa93e4cfdb817b010a
jack2684 <gjj2684@gmail.com>
2014-04-11 12:30:23 -0400

    Basic html reader and parser built complete

---
+++ src/WebWatcher.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebWatcher {
+
+  public static void main(String[] args) {
+
+    Document doc;
+    try {
+
+      // need http protocol
+      doc = Jsoup.connect("http://192.168.8.143/artxchange/drupal/").get();
+
+      // get page title
+      String title = doc.title();
+      System.out.println("title : " + title);
+
+/*      // get all links
+      Elements links = doc.select("a[href]");
+      for (Element link : links) {
+
+        // get the value from href attribute
+        System.out.println("\nlink : " + link.attr("href"));
+        System.out.println("text : " + link.text());
+
+      }*/
+
+      // try to get the body
+      Elements body = doc.select("body");
+      System.out.println(body);
+      System.out.println("done");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
