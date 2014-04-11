7a15352b7e03ff523c98f862958d344c83ea98b9
jack2684 <gjj2684@gmail.com>
2014-04-11 13:41:53 -0400

    Knowing the structure of the website

--- src/WebWatcher.java
+++ src/WebWatcher.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,16 +11,20 @@ import org.jsoup.select.Elements;
 public class WebWatcher {
 
   public static void main(String[] args) {
+    int INTERVAL = 2500; // ms
 
     Document doc;
     try {
 
       // need http protocol
-      doc = Jsoup.connect("http://192.168.8.143/artxchange/drupal/").get();
+//      String url = "http://www.dartmouthre.com/dartmouth-rentals/";
+      String url = "http://192.168.8.143/";
+      doc = Jsoup.connect(url).get();
 
       // get page title
       String title = doc.title();
       System.out.println("title : " + title);
+      System.out.println("url : " + url);
 
 /*      // get all links
       Elements links = doc.select("a[href]");
@@ -30,13 +37,38 @@ public class WebWatcher {
       }*/
 
       // try to get the body
-      Elements body = doc.select("body");
-      System.out.println(body);
-      System.out.println("done");
+      int old_hash = get_html_hash(url);
+      while (true) {
+        Thread.sleep(INTERVAL);
+        int new_hash = get_html_hash(url);
+        if (new_hash != old_hash) {
+          System.out.println("Updated!!");
+          old_hash = new_hash;
+        } else {
+          System.out.println("nothing...");
+        }
+      }
 
     } catch (IOException e) {
       e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
+  public static void logging(String str_old, String str_new) throws FileNotFoundException, UnsupportedEncodingException {
+    PrintWriter writer = new PrintWriter("old.txt", "UTF-8");
+    writer.println(str_old);
+    writer.close();
+    writer = new PrintWriter("new.txt", "UTF-8");
+    writer.println(str_new);
+    writer.close();
+  }
+
+  public static int get_html_hash(String url) throws IOException {
+    Document doc = Jsoup.connect(url).get();
+    int num_p = doc.select("p").size();
+    int num_a = doc.select("a").size();
+    return num_p * 1000 + num_a;
   }
 }
